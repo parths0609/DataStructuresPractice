919d4889f6fe864b06a5437a02cd910d9f753d75
Parth Shekokar <shekokarparth@gmail.com>
2021-05-22 18:01:43 +0530

    Add project files.

---
+++ Arrays/ArrayOperations.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeCodes.Arrays
+{
+    class ArrayOperations
+    {
+
+        #region ReverseArray
+
+        //standard answer -- Easier way with less time complexity
+        public void ReverseArray(int[] input, int start, int end)
+        {
+            int temp = 0;
+            Console.WriteLine("----REVERSE ARRAY USING STANDARD APPROACH----");
+            Console.WriteLine("---INPUT ARRAY----");
+            PrintArray(input);
+            while (start < end)
+            {
+                temp = input[start];
+                input[start] = input[end];
+                input[end] = temp;
+                start++;
+                end--;
+            }
+            Console.WriteLine("---OUTPUT ARRAY OF REVERSED INPUT----");
+            PrintArray(input);
+
+
+        }
+
+        //Recursive Approach
+        public void RecursiveReverseArray(int[] input, int start, int end)
+        {
+           
+            int temp;
+            if (start >= end)
+                return;
+            temp = input[start];
+            input[start] = input[end];
+            input[end] = temp;
+
+            RecursiveReverseArray(input, start + 1, end - 1);
+            //Console.WriteLine("----OUTPUT ARRAY OF REVERSED INPUT----");
+            //PrintArray(input);
+
+        }
+
+        //What I tried during practice. - higher time complexity
+        public void WorseCaseReverseArray(int[] input)
+        {
+            Console.WriteLine("----REVERSE ARRAY USING WORSE CASE APPROACH----");
+            Console.WriteLine("----INPUT ARRAY----");
+            PrintArray(input);
+            int[] output = new int[input.Length];
+            int j = 0;
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+
+                output[j] = input[i];
+                j++;
+            }
+
+            Console.WriteLine("----OUTPUT ARRAY OF REVERSED INPUT----");
+            PrintArray(output);
+        }
+
+        #endregion
+
+        #region DetectDuplicates
+        public void DetectDuplicates(string input)
+        {
+            Console.WriteLine("----ATTEMPTING TO DETECT DUPLICATES----");
+            HashSet<char> uniqueForm = new HashSet<char>();
+            char[] splitForm = input.ToCharArray();
+            foreach (var item in splitForm)
+            {
+                uniqueForm.Add(item);
+            }
+            Console.WriteLine("----STRING WITHOUT DUPICATES----");
+            foreach (var item in uniqueForm)
+            {
+                Console.Write(item);
+            }
+
+            
+        }
+
+        #endregion
+
+        #region Maximum&MinimumElement
+
+        //public void MaximumAndMinimum(int[] input)
+        //{
+        //    int box1 = 0; int box2 = 0;
+        //    Console.WriteLine("----EXECUTING MAXIMUM AND MINIMUM FUNCTION");
+            
+
+        //}
+
+        #endregion
+
+        #region Printing
+
+        public void PrintArray(int[] arr)
+        {
+            Console.WriteLine("-----PRINTING-----");
+            foreach (var item in arr)
+            {
+                Console.Write("[ "+item+" ]");
+            }
+            Console.Write("\n");
+            Console.WriteLine("-----------------------");
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using PracticeCodes.Arrays;
+using System;
+using System.Collections.Generic;
+
+namespace PracticeCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Objects
+            ArrayOperations operationsObj = new ArrayOperations();
+            #endregion
+
+            #region DetectDuplicates
+                Console.WriteLine("----Search for Duplicates----");
+                operationsObj.DetectDuplicates("SAMANTHA");
+            #endregion
+
+            #region ReverseArray
+
+                #region StandardWay
+            int[] param = new int[] { 1, 3, 4, 6 };
+            operationsObj.ReverseArray(param, 0, 3);
+            #endregion
+
+                #region WorseCase
+           
+            int[] inputArr = new int[] { 1, 3, 4, 6, 8 };
+            operationsObj.WorseCaseReverseArray(inputArr);
+            #endregion
+
+                #region RecursiveWay
+            operationsObj.RecursiveReverseArray(inputArr, 0, 4);
+            #endregion
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
